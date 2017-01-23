c373e3f1135549d76c9dbee9f3e44f3451fb219d
Constantine Davantzis <c.davantzis@gmail.com>
2017-01-23 18:39:56 -0500

    Improve Tests

--- TriangleExample.py
+++ TriangleExample.py
@@ -28,23 +28,25 @@ def classifyTriangle(a, b, c, angle_precision = 2):
     :rtype: str
 
     """
+    # Assert Properties of sides of a triangle
+    assert a + b >= c
+    assert a + c >= b
+    assert b + c >= a
 
+    # Calculate Angles
     a, b, c, = float(a), float(b), float(c)
     angle_a = round(degrees(acos((b * b + c * c - a * a) / (2 * b * c))), angle_precision)
     angle_b = round(degrees(acos((c * c + a * a - b * b) / (2 * a * c))), angle_precision)
     angle_c = round(180.0 - angle_a - angle_b, angle_precision)
     angles = [angle_a, angle_b, angle_c]
 
-    print angles
-
+    # Classify Triangle
     if a == b == c:
         return "Equilateral"
-
     if a == b or b == c or a == c:
         if 90.0 in angles:
             return "Isosceles Right"
         return "Isosceles"
-
     if 90.0 in angles:
         return "Scalene Right"
     return "Scalene"
@@ -60,18 +62,15 @@ class ClassifyTriangleMethods(unittest.TestCase):
         for i in range(1, 1000, 1):
             self.assertEquals(classifyTriangle(i, i, i), "Equilateral")
 
-    #def test_isosceles(self):
-    #    a, b, c = 3, 3, 6
-    #    for i in range(1, 1000, 1):
-    #        self.assertEquals(classifyTriangle(a * i, b * i, c * i), "Isosceles")
+    def test_isosceles(self):
+        """ Test case for an isosceles non-right triangle
 
-    #    a, b, c = 4, 4, 5
-    #    for i in range(1, 1000, 1):
-    #        self.assertEquals(classifyTriangle(a * i, b * i, c * i), "Isosceles")
-
-    #    a, b, c = 4, 4, 6
-    #    for i in range(1, 1000, 1):
-    #        self.assertEquals(classifyTriangle(a * i, b * i, c * i), "Isosceles")
+            In an isosceles right triangle the sides are in the ratio 1:1:sqrt(2)
+        """
+        for a in range(1, 1000, 1):
+            for c in range(a+1, 10, 1):
+                if (2*a != sqrt(c)) and (a + a > c):
+                    self.assertEquals(classifyTriangle(a, a, c), "Isosceles")
 
     def test_isosceles_right(self):
         """ Test case for an isosceles right triangle
@@ -82,20 +81,28 @@ class ClassifyTriangleMethods(unittest.TestCase):
         for i in range(1, 1000, 1):
             self.assertEquals(classifyTriangle(a, b, sqrt(c)), "Isosceles Right")
 
-    #def test_scalene(self):
-    #    a, b, c = 8, 6, 7
-    #    for i in range(1, 1000, 1):
-    #        self.assertEquals(classifyTriangle(a*i, b*i, c*i), "Scalene")
+    def test_scalene(self):
+        """ Test case for an scalene triangles
 
-    #    a, b, c = 8, 6, 14
-    #    for i in range(1, 1000, 1):
-    #        self.assertEquals(classifyTriangle(a * i, b * i, c * i), "Scalene")
+        """
+        # TODO: Is there a formula or theorem for generating non-right scalene triangles?
+        a, b, c = 8, 6, 7
+        for i in range(1, 1000, 1):
+            self.assertEquals(classifyTriangle(a*i, b*i, c*i), "Scalene")
+
+        a, b, c = 8, 6, 14
+        for i in range(1, 1000, 1):
+            self.assertEquals(classifyTriangle(a * i, b * i, c * i), "Scalene")
 
     def test_scalene_right(self):
-        for n in range(1, 1000):
-            for m in range(n + 1, 1000):
-                self.assertEquals(classifyTriangle(2 * m * n, m * m - n * n, m * m + n * n), "Scalene Right")
+        """ Test case for an scalene right triangles
 
+            Use formula for pythagorean triples for any m and n, such that m>n,
+            (2 * m * n, m * m - n * n, m * m + n * n) is a pythagorean triple
+        """
+        for n in range(1, 50):
+            for m in range(n + 1, 50):
+                self.assertEquals(classifyTriangle(2 * m * n, m * m - n * n, m * m + n * n), "Scalene Right")
 
 if __name__ == "__main__":
     suite = unittest.TestLoader().loadTestsFromTestCase(ClassifyTriangleMethods)
