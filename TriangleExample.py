aec71f8bce1a5f72d7595c1afcd93615fc78d50b
Constantine Davantzis <c.davantzis@gmail.com>
2017-01-23 17:37:49 -0500

    Improve Tests

--- TriangleExample.py
+++ TriangleExample.py
@@ -1,8 +1,8 @@
 import unittest
-from math import acos, degrees
+from math import acos, degrees, sqrt
 
 
-def classifyTriangle(a, b, c):
+def classifyTriangle(a, b, c, angle_precision = 2):
     """ Classify a triangle given the length of its 3 sides
 
         Equilateral Triangle:
@@ -20,6 +20,7 @@ def classifyTriangle(a, b, c):
     :param a: Length of side a
     :param b: Length of side b
     :param c: Length of side c
+    :param angle_precision: number of decimal places to round angles
 
     :return: whether the triangle is scalene, isosceles, or equilateral,
     and whether it is a right triangle as well.
@@ -29,12 +30,12 @@ def classifyTriangle(a, b, c):
     """
 
     a, b, c, = float(a), float(b), float(c)
-    angle_a = round(degrees(acos((b * b + c * c - a * a) / (2 * b * c))), 3)
-    angle_b = round(degrees(acos((c * c + a * a - b * b) / (2 * a * c))), 3)
-    angle_c = round(180.0 - angle_a - angle_b, 3)
+    angle_a = round(degrees(acos((b * b + c * c - a * a) / (2 * b * c))), angle_precision)
+    angle_b = round(degrees(acos((c * c + a * a - b * b) / (2 * a * c))), angle_precision)
+    angle_c = round(180.0 - angle_a - angle_b, angle_precision)
     angles = [angle_a, angle_b, angle_c]
 
-    # print angles
+    print angles
 
     if a == b == c:
         return "Equilateral"
@@ -52,32 +53,48 @@ def classifyTriangle(a, b, c):
 class ClassifyTriangleMethods(unittest.TestCase):
 
     def test_equilateral(self):
+        """ Test case for an equilateral triangle
+
+            In an equilateral triangle triangle the sides are in the ratio 1:1:1
+        """
         for i in range(1, 1000, 1):
             self.assertEquals(classifyTriangle(i, i, i), "Equilateral")
 
     #def test_isosceles(self):
-    #    self.assertEquals(classifyTriangle(7, 8, 4), "Isosceles")
+    #    a, b, c = 3, 3, 6
+    #    for i in range(1, 1000, 1):
+    #        self.assertEquals(classifyTriangle(a * i, b * i, c * i), "Isosceles")
 
-    #def test_isosceles_right(self):
-    #    pass
+    #    a, b, c = 4, 4, 5
+    #    for i in range(1, 1000, 1):
+    #        self.assertEquals(classifyTriangle(a * i, b * i, c * i), "Isosceles")
+
+    #    a, b, c = 4, 4, 6
+    #    for i in range(1, 1000, 1):
+    #        self.assertEquals(classifyTriangle(a * i, b * i, c * i), "Isosceles")
+
+    def test_isosceles_right(self):
+        """ Test case for an isosceles right triangle
+
+            In an isosceles right triangle the sides are in the ratio 1:1:sqrt(2)
+        """
+        a, b, c = 1, 1, 2
+        for i in range(1, 1000, 1):
+            self.assertEquals(classifyTriangle(a, b, sqrt(c)), "Isosceles Right")
 
     #def test_scalene(self):
-    #    self.assertEquals(classifyTriangle(8, 6, 7), "Scalene")
+    #    a, b, c = 8, 6, 7
+    #    for i in range(1, 1000, 1):
+    #        self.assertEquals(classifyTriangle(a*i, b*i, c*i), "Scalene")
+
+    #    a, b, c = 8, 6, 14
+    #    for i in range(1, 1000, 1):
+    #        self.assertEquals(classifyTriangle(a * i, b * i, c * i), "Scalene")
 
     def test_scalene_right(self):
-        # Test Pythagorean Triples
-        
-        a, b, c = 3, 4, 5
-        for i in range(1, 1000, 1):
-            self.assertEquals(classifyTriangle(a*i, b*i, c*i), "Scalene Right")
-
-        a, b, c = 5, 12, 13
-        for i in range(1, 1000, 1):
-            self.assertEquals(classifyTriangle(a * i, b * i, c * i), "Scalene Right")
-
-        a, b, c = 8, 15, 17
-        for i in range(1, 1000, 1):
-            self.assertEquals(classifyTriangle(a * i, b * i, c * i), "Scalene Right")
+        for n in range(1, 1000):
+            for m in range(n + 1, 1000):
+                self.assertEquals(classifyTriangle(2 * m * n, m * m - n * n, m * m + n * n), "Scalene Right")
 
 
 if __name__ == "__main__":
