aafd63ff939579c718779ee764238dd716663ead
Constantine Davantzis <c.davantzis@gmail.com>
2017-01-23 18:57:06 -0500

    Create seperate function for generating triangles

--- TriangleExample.py
+++ TriangleExample.py
@@ -52,57 +52,81 @@ def classifyTriangle(a, b, c, angle_precision = 2):
     return "Scalene"
 
 
+def generate_equilateral_triangles():
+    """ Generate Equilateral Triangles """
+    for i in range(1, 1000, 1):
+        yield i, i, i
+
+
+def generate_isosceles_triangles():
+    """ Generate isosceles non-right triangles
+    In an isosceles right triangle the sides are in the ratio 1:1:sqrt(2)
+    """
+    for a in range(1, 1000, 1):
+        for c in range(a + 1, 10, 1):
+            if (2 * a != sqrt(c)) and (a + a > c):
+                yield a, a, c
+
+
+def generate_isosceles_right_triangles():
+    """ Generate isosceles triangles
+    In an isosceles right triangle the sides are in the ratio 1:1:sqrt(2)
+    """
+    a, b, c = 1, 1, 2
+    for i in range(1, 1000, 1):
+        yield a * i, b * i, sqrt(c) * i
+
+
+def generate_scalene_triangles():
+    """ Generate scalene non-right triangles """
+    # TODO: Is there a formula or theorem for generating non-right scalene triangles?
+    a, b, c = 8, 6, 7
+    for i in range(1, 500, 1):
+        yield a * i, b * i, c * i
+
+    a, b, c = 8, 6, 14
+    for i in range(1, 500, 1):
+        yield a * i, b * i, c * i
+
+
+def generate_scalene_right_triangles():
+    """ Generate scalene right triangles
+
+        Use formula for pythagorean triples for any m and n, such that m>n,
+        (2 * m * n, m * m - n * n, m * m + n * n) is a pythagorean triple
+    """
+    for n in range(1, 50):
+        for m in range(n + 1, 50):
+            yield 2 * m * n, m * m - n * n, m * m + n * n
+
+
 class ClassifyTriangleMethods(unittest.TestCase):
 
     def test_equilateral(self):
-        """ Test case for an equilateral triangle
-
-            In an equilateral triangle triangle the sides are in the ratio 1:1:1
-        """
-        for i in range(1, 1000, 1):
-            self.assertEquals(classifyTriangle(i, i, i), "Equilateral")
+        """ Test case for an equilateral triangles """
+        for triangle in generate_equilateral_triangles():
+            self.assertEquals(classifyTriangle(*triangle), "Equilateral")
 
     def test_isosceles(self):
-        """ Test case for an isosceles non-right triangle
-
-            In an isosceles right triangle the sides are in the ratio 1:1:sqrt(2)
-        """
-        for a in range(1, 1000, 1):
-            for c in range(a+1, 10, 1):
-                if (2*a != sqrt(c)) and (a + a > c):
-                    self.assertEquals(classifyTriangle(a, a, c), "Isosceles")
+        """ Test case for an isosceles non-right triangles """
+        for triangle in generate_isosceles_triangles():
+            self.assertEquals(classifyTriangle(*triangle), "Isosceles")
 
     def test_isosceles_right(self):
-        """ Test case for an isosceles right triangle
-
-            In an isosceles right triangle the sides are in the ratio 1:1:sqrt(2)
-        """
-        a, b, c = 1, 1, 2
-        for i in range(1, 1000, 1):
-            self.assertEquals(classifyTriangle(a, b, sqrt(c)), "Isosceles Right")
+        """ Test case for an isosceles right triangles """
+        for triangle in generate_isosceles_right_triangles():
+            self.assertEquals(classifyTriangle(*triangle), "Isosceles Right")
 
     def test_scalene(self):
-        """ Test case for an scalene triangles
-
-        """
-        # TODO: Is there a formula or theorem for generating non-right scalene triangles?
-        a, b, c = 8, 6, 7
-        for i in range(1, 1000, 1):
-            self.assertEquals(classifyTriangle(a*i, b*i, c*i), "Scalene")
-
-        a, b, c = 8, 6, 14
-        for i in range(1, 1000, 1):
-            self.assertEquals(classifyTriangle(a * i, b * i, c * i), "Scalene")
+        """ Test case for an scalene non-right triangles """
+        for triangle in generate_scalene_triangles():
+            self.assertEquals(classifyTriangle(*triangle), "Scalene")
 
     def test_scalene_right(self):
-        """ Test case for an scalene right triangles
+        """ Test case for an scalene right triangles"""
+        for triangle in generate_scalene_right_triangles():
+                self.assertEquals(classifyTriangle(*triangle), "Scalene Right")
 
-            Use formula for pythagorean triples for any m and n, such that m>n,
-            (2 * m * n, m * m - n * n, m * m + n * n) is a pythagorean triple
-        """
-        for n in range(1, 50):
-            for m in range(n + 1, 50):
-                self.assertEquals(classifyTriangle(2 * m * n, m * m - n * n, m * m + n * n), "Scalene Right")
 
 if __name__ == "__main__":
     suite = unittest.TestLoader().loadTestsFromTestCase(ClassifyTriangleMethods)
