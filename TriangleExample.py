919ca75694b82bff9849fa3bef35e62d39dee219
pwarmuzStvns <pwarmuz@stevens.edu>
2017-01-22 19:06:13 -0500

    adjusted
    adjusted numbers

--- TriangleExample.py
+++ TriangleExample.py
@@ -1,8 +1,8 @@
 import math
 
-valuesA = [8, 3, 7, 13, 11]
-valuesB = [6, 3, 7, 13, 11]
-valuesC = [7, 3, 7, 13, 11]
+valuesA = [8, 3, 7, 3, 6]
+valuesB = [6, 3, 8, 4, 8]
+valuesC = [7, 3, 7, 5, 10]
 
 def classifyTriangle(a,b,c):
     cosA = ( math.pow(b, 2) + math.pow(c, 2) - math.pow(a, 2)) / (2*b*c)
@@ -12,9 +12,10 @@ def classifyTriangle(a,b,c):
     degreeA = round(math.degrees(radianA), 1)
     degreeB = round(math.degrees(radianB), 1)
     degreeC = 180 - (degreeA + degreeB)
+    print degreeA, degreeB, degreeC
     if degreeA == degreeB == degreeC:
         typeTriangle = "Equilateral Triangle"
-    elif degreeA == 90 or degreeB == 90 or degreeC == 90:
+    elif degreeA == 90.0 or degreeB == 90.0 or degreeC == 90.0:
         typeTriangle = "Right Triangle"
     elif degreeA == degreeB or degreeA == degreeC:
         typeTriangle = "Isosceles Triangle"
@@ -22,6 +23,6 @@ def classifyTriangle(a,b,c):
         typeTriangle = "Scalene Triangle"
     return typeTriangle
 
-for i in range(0,5):
+for i in range(0,len(valuesA)):
     response = classifyTriangle(valuesA[i], valuesB[i], valuesC[i])
     print "Line A: ", valuesA[i], "Line B: ", valuesB[i], "Line C: ", valuesC[i], " make a ", response
