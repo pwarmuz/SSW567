159782248be36cd1785f9e68a53e131e69656b31
Constantine Davantzis <c.davantzis@gmail.com>
2017-01-29 15:57:55 -0500

    Improve test cases

--- assignment_2/test_cases.py
+++ assignment_2/test_cases.py
@@ -9,28 +9,41 @@ class TestTriangles(unittest.TestCase):
 
     def test_case_00(self):
         """ Validate Right Triangle -PW"""
-        self.assertEqual(self.func(3, 4, 5), 'Right', '3,4,5 is a Right triangle')
+        self.assertEqual(self.func(3, 4, 5), 'Right', '3, 4, 5 Should Be Right')
 
     def test_case_01(self):
         """ Validate Equilateral Triangle -PW"""
-        self.assertEqual(self.func(1, 1, 1), 'Equilateral', '1,1,1 should be equilateral')
+        self.assertEqual(self.func(1, 1, 1), 'Equilateral', '1, 1, 1 Should be Equilateral')
 
     def test_case_02(self):
         """ Validate Isosceles Triangle -PW"""
-        self.assertNotEqual(self.func(10, 10, 10), 'Isosceles', 'Should be Equilateral')
+        self.assertNotEqual(self.func(10, 10, 10), 'Isosceles', '10, 10, 10 Should be Equilateral')
 
     def test_case_03(self):
         """ Validate Scalene Triangle """
-        self.assertEqual(self.func(10, 15, 30), 'Scalene', 'Should be Isoceles')
+        self.assertEqual(self.func(8, 6, 7), 'Scalene', '8, 6, 7 Should be Scalene')
 
     def test_case_04(self):
         """ Check Values, if a or b or c > 200 should = 'InvalidInput' """
+        self.assertEqual(self.func(195, 10, 201), 'InvalidInput', '195, 10, 201 Should be InvalidInput')
+        self.assertEqual(self.func(10, 201, 195), 'InvalidInput', '10, 201, 195 Should be InvalidInput')
+        self.assertEqual(self.func(201, 195, 10), 'InvalidInput', '201, 195, 10 Should be InvalidInput')
 
     def test_case_05(self):
         """ Check Values, if a or b or c <= 0 should = 'InvalidInput' """
+        self.assertEqual(self.func(-2, 2, 3), 'InvalidInput', '-2, 2, 3 Should be InvalidInput')
+        self.assertEqual(self.func(2, -2, 3), 'InvalidInput', '2, -2, 3 Should be InvalidInput')
+        self.assertEqual(self.func(2, 2, -3), 'InvalidInput', '2, 2, -3 Should be InvalidInput')
+
+        self.assertEqual(self.func(0, 2, 3), 'InvalidInput', '0, 2, 3 Should be InvalidInput')
+        self.assertEqual(self.func(2, 0, 3), 'InvalidInput', '2, 0, 3 Should be InvalidInput')
+        self.assertEqual(self.func(2, 2, 0), 'InvalidInput', '2, 2, 0 Should be InvalidInput')
 
     def test_case_06(self):
         """  Check input value is integer else = 'InvalidInput' """
+        self.assertEqual(self.func("3", 3, 3), 'InvalidInput', '"3", 3, 3 Should be InvalidInput')
+        self.assertEqual(self.func(3, "3", 3), 'InvalidInput', '3, "3", 3 Should be InvalidInput')
+        self.assertEqual(self.func(3, 3, "3"), 'InvalidInput', '3, 3, "3" Should be InvalidInput')
 
     def test_case_07(self):
         """ Check that the sum of any 2 sides is less then the 3rd side, if it fails = 'NotATriangle' """
